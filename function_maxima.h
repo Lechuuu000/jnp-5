82df5389915c2029da50f03011e0c9a303b5cf5b
Lechuuu <lesiomak@gmail.com>
2021-01-02 15:51:40 +0100

    Dodanie plików

---
+++ function_maxima.h
@@ -0,0 +1,10 @@
+#ifndef JNP_5_FUNCTION_MAXIMA_H
+#define JNP_5_FUNCTION_MAXIMA_H
+
+
+template<typename A, typename V> class FunctionMaxima {
+
+};
+
+
+#endif //JNP_5_FUNCTION_MAXIMA_H
---
+++ test.cpp
@@ -0,0 +1,10 @@
+/// plik do własnego testowania
+#include <iostream>
+
+#include "function_maxima.h"
+
+int main () {
+
+
+    return 0;
+}
